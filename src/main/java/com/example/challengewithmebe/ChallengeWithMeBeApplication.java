c69b5a7c51b8675da2e47e45a0aabfa05fa6d87f
2ralumin <2ralumin@naver.com>
2024-02-01 18:10:57 +0900

    Core : 백엔드 프로젝트 환경 세팅
    백엔드 프로젝트 생성 및 초기 환경 세팅
    - Dependencies : Lombok, Spring Data JPA, Validation, Spring Web, H2 Database, MySQL Driver
    - Java 17
    - Gradle
    
    Issues : #4

---
+++ src/main/java/com/example/challengewithmebe/ChallengeWithMeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengewithmebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeWithMeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeWithMeBeApplication.class, args);
+    }
+
+}
