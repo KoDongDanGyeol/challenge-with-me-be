bac5ec251fab35b82c4b56af61e3a70943eac5c0
2ralumin <2ralumin@naver.com>
2024-02-01 18:10:57 +0900

    Core : 백엔드 프로젝트 환경 세팅
    백엔드 프로젝트 생성 및 초기 환경 세팅
    - Dependencies : Lombok, Spring Data JPA, Validation, Spring Web, H2 Database, MySQL Driver
    - Java 17
    - Gradle
    
    Issues : #4

---
+++ src/test/java/com/example/challengewithmebe/ChallengeWithMeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengewithmebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeWithMeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
