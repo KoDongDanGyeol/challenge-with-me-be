262d1a07aab971289bbf599dec5623aa997a7c07
ko <iamkokoko123@gmail.com>
2024-02-14 21:23:25 +0900

    Refactor: 쿼리메소드 작성
    기존 서비스 로직 쿼리 메소드로 대체
    Issues: #19

--- src/main/java/com/example/challengewithmebe/problem/repository/TestcaseRepository.java
+++ src/main/java/com/example/challengewithmebe/problem/repository/TestcaseRepository.java
@@ -3,5 +3,9 @@ package com.example.challengewithmebe.problem.repository;
 import com.example.challengewithmebe.problem.domain.Testcase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TestcaseRepository extends JpaRepository<Testcase, Long> {
+    List<Testcase> findByProblemIdAndHiddenIsTrue(Long problemId);
+    List<Testcase> findByProblemIdAndHiddenIsFalse(Long problemId);
 }
