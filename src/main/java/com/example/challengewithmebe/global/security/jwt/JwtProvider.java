5b38ee1c9417e8fa6701bb61476ceb02fb492de5
2ralumin <2ralumin@naver.com>
2024-02-16 01:48:33 +0900

    Feature :  시큐리티 + 토큰 설정

---
+++ src/main/java/com/example/challengewithmebe/global/security/jwt/JwtProvider.java
@@ -0,0 +1,101 @@
+package com.example.challengewithmebe.global.security.jwt;
+
+import com.example.challengewithmebe.global.security.userdetails.CustomUserDetailService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+    private final CustomUserDetailService customUserDetailService;
+    @Value("${spring.jwt.secretKey}")
+    private String secretKey;
+    public static final long TOKEN_VALID_TIME = 1000L * 60 * 60 * 24; //1일
+    public static final long REFRESH_TOKEN_VALID_TIME = 1000L * 60 * 60 * 24 * 7; //7일
+
+    @PostConstruct //초기화용
+    public void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createAccessToken(String userId){
+        Claims claims = Jwts.claims().setSubject(userId);
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeader(createHeader())
+                .setClaims(claims)
+                .setExpiration(new Date(now.getTime()+TOKEN_VALID_TIME))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String createRefreshToken(){
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime()+REFRESH_TOKEN_VALID_TIME))
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .compact();
+    }
+
+    public Authentication getAutentication(String token){
+        UserDetails userDetails = customUserDetailService.loadUserByUsername(getId(token));
+        return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
+    }
+
+    public String getId(String token){
+        try{
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        }catch (ExpiredJwtException e){
+            return e.getClaims().getSubject();
+        }
+    }
+
+    public String resolveToken(HttpServletRequest request){
+        String bearer = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(bearer!=null && bearer.startsWith("Bearer "))
+            return bearer.substring("Bearer ".length());
+        return null ; //todo : 에러코드 추가
+    }
+
+    public boolean validateTokenExpirations(String token){
+        try{
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    //토큰에서 id 추출
+    public String extractId(HttpServletRequest httpServletRequest){
+        String accessToken = resolveToken(httpServletRequest);
+        return getId(accessToken);
+    }
+
+
+
+    private Map<String, Object> createHeader(){
+        Map<String, Object> header = new HashMap<>();
+        header.put("typ", "JWT");
+        header.put("alg", "HS256");
+        return header;
+    }
+}
