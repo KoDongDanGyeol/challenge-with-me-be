028d198e98ef01d83321ef8544ce6d87730cdaa1
2ralumin <2ralumin@naver.com>
2024-02-16 01:40:33 +0900

    Feature : MemberRepository 생성

---
+++ src/main/java/com/example/challengewithmebe/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.challengewithmebe.member.repository;
+
+import com.example.challengewithmebe.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmailAndProvider(String email, String provider);
+}
