1c223368c9f2f240365f3641d19f4cda293db526
jjw0071 <jjw0071@naver.com>
2024-02-23 15:05:10 +0900

    Feature : QuestionRepositry에서 문제 ID 접근 방식 변경
    Issues : #51

--- src/main/java/com/example/challengewithmebe/qna/repository/QuestionRepository.java
+++ src/main/java/com/example/challengewithmebe/qna/repository/QuestionRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
-    List<Question> findByProblemIdOrderByModifiedAtDesc(Long problemId);
+    List<Question> findByProblemId_IdOrderByModifiedAtDesc(Long problemId);
 
     // 특정 문제에 대한 질문들 페이지로 넘김
-    Page<Question> findByProblemId(Long problemId, Pageable pageable);
+    Page<Question> findByProblemId_Id(Long problemId, Pageable pageable);
 
     // 특정 문제에 대한 질문 중 특정 사용자가 작성한 질문들만 페이지로 넘김
-    Page<Question> findByProblemIdAndMemberId_Id(Long problemId,Long memberId, Pageable pageable);
+    Page<Question> findByProblemId_IdAndMemberId_Id(Long problemId,Long memberId, Pageable pageable);
 
     // 모든 질문 중 특정 사용자가 작성한 질문들만 페이지로 넘김
     Page<Question> findByMemberId_Id(Long MemberId,Pageable pageable);
