ffa67b7dff46a121a73303cd6e6688f6d0856fbb
MINSEON\minseon <duiph558291@gmail.com>
2024-02-26 16:11:46 +0900

    Feature : 정답률 및 완료한 사람 수 문제 목록 포함 구현

--- src/main/java/com/example/challengewithmebe/submit/domain/Submit.java
+++ src/main/java/com/example/challengewithmebe/submit/domain/Submit.java
@@ -1,6 +1,5 @@
 package com.example.challengewithmebe.submit.domain;
 
-import com.example.challengewithmebe.global.app.domain.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,15 +14,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Submit extends BaseEntity {
+public class Submit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long problemId; //문제id
     private Long memberId; //회원id
     private boolean isCorrect; //정답여부
     private String submitCode; //제출코드
     private String language; //언어
-    private String status; //푼 문제, 안 푼 문제
+    private String status; //문제 상태?
+
+    public Submit(Long problemId, Long memberId, boolean isCorrect, String code) {
+        this.problemId = problemId;
+        this.memberId = memberId;
+        this.isCorrect = isCorrect;
+        this.submitCode = code;
+    }
 }
